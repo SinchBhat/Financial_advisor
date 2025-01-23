41bfd784c85f0569fa6a0160bcb951db244961ef
SinchBhat <sinchana3.mitmpl2023@learner.manipal.edu>
2025-01-23 15:32:55 +0530

    added middleware.js

--- finance_portfolio/app/layout.js
+++ finance_portfolio/app/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${inika.className}`}>
@@ -27,5 +28,6 @@ export default function RootLayout({ children }) {
       </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
