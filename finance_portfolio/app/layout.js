610dc546db3e318c0ab2d646be4cd43ddc359c47
SinchBhat <sinchana3.mitmpl2023@learner.manipal.edu>
2025-01-23 14:54:15 +0530

    Added header

--- finance_portfolio/app/layout.js
+++ finance_portfolio/app/layout.js
@@ -1,5 +1,6 @@
 import { Inika } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inika=Inika({subsets:["Latin"],weight:["400"]})
 export const metadata = {
@@ -11,9 +12,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inika.className}`}
-      >
-        {children}
+        className={`${inika.className}`}>
+      {/* header*/}
+      <Header/>
+      <main className="min-h-screen">
+      {children}
+      </main>
+     
+      {/*footer*/}      
+      <footer className="bg-blue-50 py-12" >
+      <div className="container mx-auto px-4 text-center text-gray-600">
+        <p>Made with love by Sinchana</p>
+      </div>
+      </footer>
       </body>
     </html>
   );
